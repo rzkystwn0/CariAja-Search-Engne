4258a6f9696f68ac9027378a3a4037f62cf4ad9f
Rizky Setiawan || Vincent <103617822+rzkystwn0@users.noreply.github.com>
2022-09-28 21:54:29 +0700

    of dark mode

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="styleheet" />
       </Head>
-      <body className="dark:bg-black">
+      <body>
         <Main />
         <NextScript />
       </body>
